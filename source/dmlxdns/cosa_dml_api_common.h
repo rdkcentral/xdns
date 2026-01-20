123baf38bdecae854b4f5c0053d76d690dfa47b8
Goutam Damodaran <140494857+GoutamD2905@users.noreply.github.com>
2026-01-20 16:21:54 +0530

    updating code

--- source/dmlxdns/cosa_dml_api_common.h
+++ source/dmlxdns/cosa_dml_api_common.h
@@ -126,26 +126,7 @@ _COSA_DML_STATUS
 COSA_DML_STATUS, *PCOSA_DML_STATUS;
 
 #ifdef _ONESTACK_PRODUCT_REQ_
-
-bool is_bci_partner(void);
-
-/* Business Gateway - runtime partner check */
-#define BCI_PRODUCT_CHECK if (is_bci_partner()) \
-     {
-
-#define BCI_PRODUCT_CHECK_END }
-
-/* Residential Gateway - runtime partner check (inverse) */
-#define RES_PRODUCT_CHECK if (!is_bci_partner()) \
-     {
-
-#define RES_PRODUCT_CHECK_END }
-
-#else
-#define BCI_PRODUCT_CHECK
-#define BCI_PRODUCT_CHECK_END
-#define RES_PRODUCT_CHECK
-#define RES_PRODUCT_CHECK_END
+#include "common_onestack.h"
 #endif
 
 #endif
--- source/dmlxdns/cosa_xdns_apis.c
+++ source/dmlxdns/cosa_xdns_apis.c
@@ -103,23 +103,6 @@ extern ANSC_HANDLE bus_handle;
 #include "linux/if.h"
 #endif
 
-#if defined(_ONESTACK_PRODUCT_REQ_)
-#define BUFLEN_32 32
-static char partnerID[BUFLEN_32] = {0};
-
-bool is_bci_partner(void)
-{
-    if (partnerID[0] == '\0')
-    {
-        if (syscfg_get(NULL, "PartnerID", partnerID, sizeof(partnerID)) != 0)
-        {
-            return false;
-        }
-    }
-    return strcmp(partnerID, "comcast-business") == 0;
-}
-#endif
-
 void* MonitorResolvConfForChanges(void *arg);
 
 STATIC void eventReceiveHandler(
